b44b7374f881dea4e0a700f15ceefc54a9d66fa5
Matthew Christopher Albert <matthewmcavaio@gmail.com>
2022-04-27 01:11:58 +0700

    Initial Commit

---
+++ VODI/VODIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VODIApp.swift
+//  VODI
+//
+//  Created by Matthew Christopher Albert on 27/04/22.
+//
+
+import SwiftUI
+
+@main
+struct VODIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
